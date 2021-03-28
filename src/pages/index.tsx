13e267265655188b66bd8fd8a7a1414dcac0d04a
fernandomarca <fernandomarca@hotmail.com>
2021-03-28 20:21:33 -0300

    page home and post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,61 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className={styles.container}>
+        <header>
+          <img src="/logo.svg" alt="logo" />
+        </header>
+        <main>
+          <article>
+            <h1>Como utilizar Hooks</h1>
+            <h4>Pensando em sincronização em vez de ciclos de vida.</h4>
+            <div className={styles.info}>
+              <div>
+                <FiCalendar />
+                <span>15 Mar 2021</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Fernando Marca</span>
+              </div>
+            </div>
+          </article>
+          <article>
+            <h1>Criando um app CRA do zero</h1>
+            <h4>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </h4>
+            <div className={styles.info}>
+              <div>
+                <FiCalendar />
+                <span>15 Mar 2021</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Fernando Marca</span>
+              </div>
+            </div>
+          </article>
+          <button
+            type="button"
+            onClick={() => {
+              console.log('carregando...');
+            }}
+          >
+            Carregar mais posts
+          </button>
+        </main>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
